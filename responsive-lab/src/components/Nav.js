28a7236465105969ebec07514adbeca825e2c291
Daniel Cope <danielcope@users.noreply.github.com>
2021-03-23 09:13:21 -0700

    added some styling

--- responsive-lab/src/components/Nav.js
+++ responsive-lab/src/components/Nav.js
@@ -9,24 +9,33 @@ class Nav extends Component {
     }
 }
 
+
+  clickHandler = () => {
+    this.setState({ menu: !this.state.menu })
+  }
+
+  // menu = () => {
+  //   if (this.state.menu === false) {
+
+  //   }
+  // }
+
   render(){
+
+    const {menu} = this.state
+
     return (
-      <nav>
-        <button className='ham-button'>menu
-        <div className='hamburger'>
-          <div className='ham'></div>
-          <div className='ham'></div>
-          <div className='ham'></div>
-        </div>
-        </button>
-        <ul>
-          <li>services</li>
-          <li>portfolio</li>
-          <li>about</li>
-          <li>team</li>
-          <li>contact</li>
-        </ul>
-      </nav>
+      <section>
+        <button onClick={this.clickHandler} className='ham-button'>menu
+        <span>&#9776;</span></button>
+        <nav className={ menu ? 'opened' : 'closed'} >
+          <p>services</p>
+          <p>portfolio</p>
+          <p>about</p>
+          <p>team</p>
+          <p>contact</p>
+        </nav>
+      </section>
     )
   }
 }
